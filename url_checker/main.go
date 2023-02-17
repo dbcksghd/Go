c9195ccd2a9a7fbadb40d679e799c0515f312cc9
desktop yoochanhong <chanhongy6@naver.com>
2023-02-17 23:58:29 +0900

    for문 돌면서 하나하나 확인하기

--- url_checker/main.go
+++ url_checker/main.go
@@ -9,6 +9,8 @@ import (
 var errRequestFailed = errors.New("불러오기 실패")
 
 func main() {
+
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.naver.com/",
 		"https://www.youtube.com/",
@@ -16,15 +18,25 @@ func main() {
 		"https://www.acmicpc.net/",
 		"https://www.google.co.kr/",
 	}
+
 	for _, url := range urls {
-		fmt.Println("checking ", url)
-		hitURL(url)
+		fmt.Println(url + "확인 중...")
+		result := "성공"
+		err := hitURL(url)
+		if err != nil {
+			result = "실패"
+		}
+		results[url] = result
+	}
+	for url, res := range results {
+		fmt.Println(url, res)
 	}
 }
 
 func hitURL(url string) error {
 	res, err := http.Get(url)
 	if err != nil || res.StatusCode >= 400 {
+		fmt.Println(err, res.StatusCode)
 		return errRequestFailed
 	}
 	return nil
