8f27cefbbbca71892528d50ef6d3e7e70b76b5ca
desktop yoochanhong <chanhongy6@naver.com>
2023-02-17 22:34:30 +0900

    Update 함수 추가

--- make_dictionary_and_functions/myDict/my_dict.go
+++ make_dictionary_and_functions/myDict/my_dict.go
@@ -3,8 +3,9 @@ package myDict
 import "errors"
 
 var (
-	errNotFound   = errors.New("찾지 못했습니다.")
-	errWordExists = errors.New("이 단어는 이미 있습니다.")
+	errNotFound     = errors.New("찾지 못했습니다.")
+	errWordExists   = errors.New("이 단어는 이미 있습니다.")
+	errCannotUpdate = errors.New("없는 단어는 업뎃할 수 없습니다.")
 )
 
 type Dictionary map[string]string
@@ -24,6 +25,17 @@ func (d Dictionary) Add(word, def string) error {
 		d[word] = def
 	case nil:
 		return errWordExists
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCannotUpdate
 	}
 	return nil
 }
