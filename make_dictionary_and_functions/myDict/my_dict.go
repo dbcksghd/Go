b1b92d264a6c0dfebf9ca93dd5f918fa12d1206c
desktop yoochanhong <chanhongy6@naver.com>
2023-02-17 21:42:04 +0900

    Search 함수 추가

--- make_dictionary_and_functions/myDict/my_dict.go
+++ make_dictionary_and_functions/myDict/my_dict.go
@@ -1,3 +1,15 @@
 package myDict
 
+import "errors"
+
+var errNotFound = errors.New("찾지 못했습니다.")
+
 type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	val, exi := d[word]
+	if exi {
+		return val, nil
+	}
+	return "", errNotFound
+}
