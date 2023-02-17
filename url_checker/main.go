c92f1855d79bd0e90ce8eba0cc0b42d378b4c4e5
desktop yoochanhong <chanhongy6@naver.com>
2023-02-17 23:23:32 +0900

    URL체크 함수 만들기

--- url_checker/main.go
+++ url_checker/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("불러오기 실패")
+
 func main() {
 	urls := []string{
 		"https://www.naver.com/",
@@ -8,4 +16,16 @@ func main() {
 		"https://www.acmicpc.net/",
 		"https://www.google.co.kr/",
 	}
+	for _, url := range urls {
+		fmt.Println("checking ", url)
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	res, err := http.Get(url)
+	if err != nil || res.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
