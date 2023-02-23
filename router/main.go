fbc69c0be882cf20780fb9ab51dbe00e7c5c8af1
desktop yoochanhong <chanhongy6@naver.com>
2023-02-23 21:11:26 +0900

    모든 요청을 라우터가 처리하도록 수정

--- router/main.go
+++ router/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	r := &router{make(map[string]map[string]http.HandlerFunc)}
+
+	r.HandleFunc("GET", "/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, "welcome!")
+	})
+
+	r.HandleFunc("GET", "/what", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, "what")
+	})
+
+	r.HandleFunc("GET", "/users/:id", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, "사용자 검색")
+	})
+
+	r.HandleFunc("POST", "/users", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, "사용자 생성")
+	})
+	r.HandleFunc("POST", "/users/:user_id/addresses", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, "사용자의 주소 생성")
+	})
+
+	http.ListenAndServe(":8080", r)
+}
