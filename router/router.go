aeeeded985ad682f6e525ca467aeb7105cc975d0
desktop yoochanhong <chanhongy6@naver.com>
2023-02-23 19:48:43 +0900

    라우터에 핸들러를 등록하기 위한 메소드 정의

--- router/router.go
+++ router/router.go
@@ -7,3 +7,15 @@ type router struct {
 	//값 : url별로 실행할 핸들러 함수
 	handlers map[string]map[string]http.HandlerFunc
 }
+
+func (r *router) HandleFunc(method, pattern string, h http.HandlerFunc) {
+	// http 메소드로 등록된 키가 있는지 확인
+	m, ok := r.handlers[method]
+	if !ok {
+		// 만약 없다면 새로운 맵을 만들어서 함수를 키값에 저장
+		m = make(map[string]http.HandlerFunc)
+		r.handlers[method] = m
+	}
+	//키로 등록된 맵에 url 패턴과 핸들러 함수 등록
+	m[pattern] = h
+}
