290caecbe0ae63ec669cee9120bb701f9a85ca2e
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-16 17:30:18 +0900

    getPages 함수 만들기

--- job_scrapper/main.go
+++ job_scrapper/main.go
@@ -1,7 +1,25 @@
 package main
 
-var baseURL string = "https://kr.indeed.com/jobs?q=flutter&limit=50"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var baseURL string = "https://www.google.com/search?q=flutter&rlz=1C5CHFA_enKR1035KR1035&oq=flutter&aqs=chrome..69i57j69i59l3j69i60l4.3615j0j4&sourceid=chrome&ie=UTF-8"
 
 func main() {
+	pages := getPages()
+	fmt.Println(pages)
+}
 
+func getPages() int {
+	response, errorMessage := http.Get(baseURL)
+	if errorMessage != nil {
+		log.Fatalln(errorMessage) // 만약 에러가 있다면 로그찍고 프로그램 끝내버리기
+	}
+	if response.StatusCode != 200 {
+		log.Fatalln("에러코드 : ", response.StatusCode) // 200이 아니여도 프로그램 끝내기
+	}
+	return 0
 }
