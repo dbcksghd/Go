f8dcbd98e093cbea23895d93c90eb05973042227
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-16 17:32:42 +0900

    에러 처리 과정 함수화

--- job_scrapper/main.go
+++ job_scrapper/main.go
@@ -15,11 +15,19 @@ func main() {
 
 func getPages() int {
 	response, errorMessage := http.Get(baseURL)
+	checkErr(errorMessage)
+	checkStatusCode(response)
+	return 0
+}
+
+func checkErr(errorMessage error) {
 	if errorMessage != nil {
 		log.Fatalln(errorMessage) // 만약 에러가 있다면 로그찍고 프로그램 끝내버리기
 	}
+}
+
+func checkStatusCode(response *http.Response) {
 	if response.StatusCode != 200 {
 		log.Fatalln("에러코드 : ", response.StatusCode) // 200이 아니여도 프로그램 끝내기
 	}
-	return 0
 }
