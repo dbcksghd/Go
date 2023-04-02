2f3f8ef56e90c1909f3ed294d3e26be2502dad4d
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-02 19:42:26 +0900

    json 데이터 파싱하기 성공!

--- json_parsing/main.go
+++ json_parsing/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"json_parsing/utils"
+	"net/http"
+)
+
+type Photo struct {
+	AlbumId      int
+	Id           int
+	Title        string
+	Url          string
+	ThumbnailUrl string
+}
 
 func main() {
-	fmt.Println("Hello world")
+	response, err := http.Get("https://jsonplaceholder.typicode.com/photos")
+	utils.CheckErr(err)
+	var photos []Photo
+	err = json.NewDecoder(response.Body).Decode(&photos)
+	utils.CheckErr(err)
+	for _, photo := range photos {
+		fmt.Println(photo.Title)
+	}
 }
