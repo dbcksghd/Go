c34687b516fbc53e2ecd757120513a64ffddf671
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-12 21:53:19 +0900

    데이터 만들어서 db에 추가하기

--- crud/main.go
+++ crud/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+)
+
+type User struct {
+	UserId   int    `json:"user_id"`
+	Name     string `json:"name"`
+	Birthday string `json:"birthday"`
+}
 
 func main() {
-	fmt.Println("what")
+	password := os.Getenv("PASSWORD")
+	db, err := sql.Open("mysql", "root:"+password+"@tcp(localhost:3306)/test")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	newUser := User{UserId: 1235, Name: "길근우", Birthday: "20060412"}
+	_, err = db.Exec("INSERT INTO user (user_id, name, birthday) VALUES (?, ?, ?)", newUser.UserId, newUser.Name, newUser.Birthday)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(newUser.UserId, newUser.Name, newUser.Birthday) // 1235 길근우 20060412
 }
