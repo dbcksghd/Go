57582c92e4b72e577743db7fc1dc414e77364b70
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-14 02:02:26 +0900

    맵에 있는 값이 pwd와 같다면 로그인 성공 출력해주기

--- signin_and_login/main.go
+++ signin_and_login/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	m := make(map[string]string)
 	e := echo.New()
-	e.POST("/signin", func(c echo.Context) error {
+	e.POST("/signup", func(c echo.Context) error {
 		email := c.QueryParam("email")
 		pwd := c.QueryParam("password")
 		if i := m[email]; i != "" {
@@ -18,6 +18,14 @@ func main() {
 		val, isTrue := m[email]
 		defer fmt.Println(val, isTrue) //잘 출력해줌 !!
 		return c.NoContent(201)
+	})
+	e.POST("/signin", func(c echo.Context) error {
+		email := c.QueryParam("email")
+		pwd := c.QueryParam("password")
+		if i := m[email]; i == pwd {
+			return c.JSON(200, "로그인에 성공하셨습니다!")
+		}
+		return c.JSON(404, "실패")
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
