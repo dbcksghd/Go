c601b5893bf8c21f04874ca5835c212cb7ad5c46
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-14 01:51:52 +0900

    /signin 으로 들어온 정보를 맵에 추가하기

--- signin_and_login/main.go
+++ signin_and_login/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	m := make(map[string]string)
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.JSON(200, "what")
+	e.POST("/signin", func(c echo.Context) error {
+		email := c.QueryParam("email")
+		pwd := c.QueryParam("password")
+		m[email] = pwd
+		val, isTrue := m[email]
+		defer fmt.Println(val, isTrue) //잘 출력해줌 !!
+		return c.NoContent(200)
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
