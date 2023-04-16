e898b3648669c3aaafbac4a78acfdfe9ce4f22b5
yoochanhong <chanhongy6@naver.com>
2023-04-16 17:21:17 +0900

    유저 생성

--- signin_and_login/db_connect/main.go
+++ signin_and_login/db_connect/main.go
@@ -13,10 +13,18 @@ type User struct {
 }
 
 func main() {
+	
 	password := os.Getenv("PASSWORD")
 	db, err := sql.Open("mysql", "root:"+password+"@tcp(localhost:3306)/test")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+
+	newUser := User{user_id: 1265, name: "강민", birthday: "20060629"}
+	_, err = db.Exec("INSERT INTO user (name, user_id, birthday) VALUES (?, ?, ?)",
+		newUser.name, newUser.user_id, newUser.birthday)
+	if err != nil {
+		panic(err)
+	}
 }
