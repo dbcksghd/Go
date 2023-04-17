73b14bec49a1ff502792854ba27d554b20b988a5
yoochanhong <chanhongy6@naver.com>
2023-04-17 12:14:12 +0900

    받아올 유저 구조체 생성

--- graphql/main.go
+++ graphql/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type GithubUser struct {
+	Login     string `json:"login"`
+	AvatarURL string `json:"avatar_url"`
+	HTMLURL   string `json:"html_url"`
+}
+
 func main() {
 	fmt.Println("what")
 }
