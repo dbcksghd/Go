30f4ce52740ed4dfaaa39be4c1cb086f66016db5
yoochanhong <chanhongy6@naver.com>
2023-04-23 21:50:57 +0900

    oauth 인증 정보 정의

--- github_login_example/main.go
+++ github_login_example/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "os"
+import (
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+	"os"
+)
 
 func main() {
 	client_id := os.Getenv("client_id")
-	client_secrets := os.Getenv("client_secrets")
+	client_secret := os.Getenv("client_secret")
+
+	githubConfig := &oauth2.Config{
+		ClientID:     client_id,
+		ClientSecret: client_secret,
+		Scopes:       []string{"user:email"},
+		Endpoint:     github.Endpoint,
+	}
 }
