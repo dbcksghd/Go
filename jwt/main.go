d78af9b9572fd4882b7e00775a78460b9389ef0e
yoochanhong <chanhongy6@naver.com>
2023-04-24 22:07:37 +0900

    claims용 구조체 정의

--- jwt/main.go
+++ jwt/main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go/v4"
+)
+
+type TokenClaims struct {
+	UserID string   `json:"id"`
+	Name   string   `json:"name"`
+	Role   []string `json:"role"`
+	jwt.StandardClaims
+}
 
 func main() {
 	fmt.Println("what")
