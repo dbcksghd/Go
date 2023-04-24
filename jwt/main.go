d0e29f560d9e59b72ee0002a5f6af33cb1b65310
yoochanhong <chanhongy6@naver.com>
2023-04-24 22:17:33 +0900

    claims부분에 정보 추가하기

--- jwt/main.go
+++ jwt/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"github.com/dgrijalva/jwt-go/v4"
+	"time"
 )
 
 type TokenClaims struct {
-	UserID string   `json:"id"`
-	Name   string   `json:"name"`
-	Role   []string `json:"role"`
+	UserID string `json:"id"`
+	Name   string `json:"name"`
+	Role   string `json:"role"`
 	jwt.StandardClaims
 }
 
 func main() {
-	fmt.Println("what")
+	tc := TokenClaims{
+		UserID: "chanhong1206",
+		Name:   "yoochanhong",
+		Role:   "admin",
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: jwt.At(time.Now().Add(time.Hour)),
+		},
+	}
 }
