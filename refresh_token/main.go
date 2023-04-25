0df77792db92f0301e76227aae5e3cb146e8932c
yoochanhong <chanhongy6@naver.com>
2023-04-25 11:09:33 +0900

    로그인

--- refresh_token/main.go
+++ refresh_token/main.go
@@ -16,5 +16,14 @@ func main() {
 		m[id] = password
 		return c.NoContent(200)
 	})
+
+	e.GET("/signin", func(c echo.Context) error {
+		id := c.QueryParam("id")
+		password := c.QueryParam("password")
+		if i := m[id]; i == password {
+			return c.JSON(200, "로그인에 성공하셨습니다!")
+		}
+		return c.NoContent(404)
+	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
