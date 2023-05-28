a2e067ce9b64cfc4099813d0130a5b96b346edf3
yoochanhong <chanhongy6@naver.com>
2023-05-28 21:17:04 +0900

    db 연동

--- gorm/main.go
+++ gorm/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
 )
 
 func main() {
+	password := os.Getenv("PASSWORD")
+	dsn := "root:" + password + "@tcp(localhost:3306)/test"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("what")
 }
